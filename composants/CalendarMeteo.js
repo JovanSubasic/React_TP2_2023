0b31641ead71ef9f5decc5ce1eb4b85d624113db
JovanSubasic <105287870+JovanSubasic@users.noreply.github.com>
2023-06-07 16:57:51 +0200

    Ajout prévision semaine

--- composants/CalendarMeteo.js
+++ composants/CalendarMeteo.js
@@ -9,8 +9,11 @@ export default function CalendarMeteo() {
     const [location, setLocation] = useState(null);
     const [errorMsg, setErrorMsg] = useState(null);
 
-    const [calendarData, setCalendarData] = useState(null);
+    const [selectDay, setSelectDay] = useState(null);
 
+    const [selectHour2, setSelectHour] = useState(null);
+
+    const [calendarData, setCalendarData] = useState(null);
 
     const [calendarDataSelect, setCalendarDataSelect] = useState(null);
 
@@ -22,22 +25,43 @@ export default function CalendarMeteo() {
         const data = await response.json();
           setCalendarData(data);
           setCalendarDataSelect(data.list[0]);
-        console.log(calendarDataSelect);
-        // console.log(calendarData.list);
+        // console.log(calendarDataSelect);
+        console.log(data.list);
         } catch (error) {
         console.error(error);
         }
     };
 
     const listeJour = [
+        "Dim",
         "Lun",
         "Mar",
         "Mer", 
         "Jeu",
         "Ven",
-        "Sam",
-        "Dim"
+        "Sam"
     ];
+
+    // let removeJour = (new Date().getDay())-2;
+    //   if(removeJour == -1) removeJour = 6
+    //   if(removeJour == -2) removeJour = 6
+    //   newListeJour = listeJour.splice(removeJour, 2);
+
+    const listeHour = [
+      "03h",
+      "06h",
+      "09h",
+      "12h",
+      "15h", 
+      "18h",
+      "21h",
+      "00h"
+  ];
+
+  let selectHour = null
+
+  selectDay == null ? console.log(listeJour, listeJour[ new Date().getDay() ]) : '';
+  selectDay == null ? setSelectDay(listeJour[ new Date().getDay() ]) : '';
 
     useEffect(() => {
         
@@ -57,6 +81,11 @@ export default function CalendarMeteo() {
         location && getCalendar(location.coords.latitude,location.coords.longitude);
     }, []);
 
+    useEffect(() => {
+      setSelectHour(selectHour);
+      console.log(selectHour2)
+  }, [selectHour]);
+
 
   return (
     <View style={styles.container}>
@@ -68,22 +97,6 @@ export default function CalendarMeteo() {
             -faire un select avec les heures dispo du jour en sélectionnant la première heure dispo de ce jour
         */}
         
-        {/* <Picker
-            selectedValue={selectedValue}
-            style={{ height: 50, width: 150 }}
-            onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
-        >
-            {
-            listeJour.map((item, index) => {
-
-                return (
-                    <Picker.Item label={item} value={index} />
-                );
-            })
-            } 
-            
-            <Picker.Item label="JavaScript" value="js" />
-        </Picker> */}
         <ScrollView style={styles.scrollView}>
             
           {/* <SelectDropdown
@@ -91,16 +104,82 @@ export default function CalendarMeteo() {
             // defaultValueByIndex={1} // use default value by index or default value
             // defaultValue={'Canada'} // use default value by index or default value
             onSelect={(selectedItem, index) => {
-              console.log(selectedItem, index);
+              console.log(selectedItem, index, calendarDataSelect);
             }}
+            defaultValue={listeJour[ (new Date().getDay())-1 ]}
             defaultButtonText={'Sélectionner  un jour'}
-            buttonTextAfterSelection={(selectedItem, index) => {
-              return selectedItem;
-            }}
-            rowTextForSelection={(item, index) => {
-              return item;
-            }}
+            // buttonTextAfterSelection={(selectedItem, index) => {
+            //   return selectedItem;
+            // }}
+            // rowTextForSelection={(item, index) => {
+            //   return item;
+            // }}
           /> */}
+
+            {
+            calendarData.list.map((item, index) => {              
+              
+                const date = new Date(item.dt_txt);
+
+                let day = listeJour[date.getDay()];
+
+                let hour = date.getHours();
+
+                let view = 0;
+
+                // let selectHour = null
+                // selectHour == null ? setSelectHour(hour+'h') : '';
+
+                selectHour == null ? selectHour = hour+'h' : '';
+
+                console.log(hour+'h', selectHour2,selectHour, day, selectDay , date.getDay());
+
+                if(( hour+'h' == selectHour2 || hour+'h' == selectHour) && day == selectDay){
+                  return (
+                    
+                    <View key={index}>
+                      <SelectDropdown
+                        data={listeJour}
+                        // defaultValueByIndex={1} // use default value by index or default value
+                        // defaultValue={'Canada'} // use default value by index or default value
+                        onSelect={(selectedItem, index) => {
+                          setSelectDay(selectedItem);
+                          // setSelectHour(hour+'h');
+                          // getCalendar(location.coords.latitude,location.coords.longitude);
+                          console.log(listeJour, selectedItem);
+                        }}
+                        defaultValue={day}
+                        defaultButtonText={'Sélectionner  un jour'}
+                      /> 
+                      <Text>{ item.main.temp } °C</Text>
+                      <Image
+                          resizeMode="cover"
+                          style={styles.image}
+                          source={{
+                          uri: 'https://openweathermap.org/img/wn/'+ item.weather[0].icon +'@2x.png',
+                          }}
+                              
+                      />
+                      <Text>{ item.weather[0].description }</Text>
+                      <SelectDropdown
+                        data={listeHour}
+
+                        onSelect={(selectedItem, index) => {
+                          selectHour = selectedItem;
+                          console.log(selectedItem, selectHour);
+                        }}
+
+                        defaultValue={hour+'h'}
+
+                        defaultButtonText={'Sélectionner  un jour'}
+                      /> 
+                  </View>
+                  
+                  );
+                }
+            })
+            }
+
             {/* {
             calendarData.list.map((item, index) => {
                 
